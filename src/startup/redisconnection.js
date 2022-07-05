f146154904ff100de72781d25541c923c59cbf87
mugenga <rockyyves@gmail.com>
2022-07-05 14:54:56 +0200

    Debug redis

--- src/startup/redisconnection.js
+++ src/startup/redisconnection.js
@@ -1,23 +1,23 @@
-const redis = require('redis');
-const config = require('config');
-const logger = require('../logging');
+// const redis = require('redis');
+// const config = require('config');
+// const logger = require('../logging');
 
-const client = redis.createClient({
-  port: config.get('redis.port'),
-  host: config.get('redis.hostname'),
-  connect_timeout: config.get('redis.con_timeout'),
-  retry_strategy: 10,
-  password: config.get('redis.redis_key')
-});
+// const client = redis.createClient({
+//   port: config.get('redis.port'),
+//   host: config.get('redis.hostname'),
+//   connect_timeout: config.get('redis.con_timeout'),
+//   retry_strategy: 10,
+//   password: config.get('redis.redis_key')
+// });
 
 
-client.on('connect', () => {
-  logger.info('Connection to redis server is successful');
-});
+// client.on('connect', () => {
+//   logger.info('Connection to redis server is successful');
+// });
 
-client.on('error', err => {
-  logger.error('Error occurred while connecting to redis:' + err);
-  /// process.exit(1);
-});
+// client.on('error', err => {
+//   logger.error('Error occurred while connecting to redis:' + err);
+//   /// process.exit(1);
+// });
 
-module.exports = client;
+// module.exports = client;
