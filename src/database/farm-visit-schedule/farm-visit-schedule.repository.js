4344d732000fbd7cdcf61e355b84fd07190deb47
Rukundo Joseph <imagination081@gmail.com>
2022-07-14 16:59:17 +0200

    save

--- src/database/farm-visit-schedule/farm-visit-schedule.repository.js
+++ src/database/farm-visit-schedule/farm-visit-schedule.repository.js
@@ -5,6 +5,36 @@ class FarmVisitScheduleRepository extends BaseRepository {
     constructor(model) {
         super(model);
     }
+    find(data) {
+        return super.find(data)
+            .populate('farms.location.prov_id', 'namek')
+            .populate('farms.location.dist_id', 'name')
+            .populate('farms.location.sect_id', 'name')
+            .populate('farms.location.cell_id', 'name')
+            .populate('farms.location.village_id', 'name')
+            .populate('gaps')
+            .populate('groupId');
+    }
+    findAll() {
+        return super.findAll()
+            .populate('farms.location.prov_id', 'namek')
+            .populate('farms.location.dist_id', 'name')
+            .populate('farms.location.sect_id', 'name')
+            .populate('farms.location.cell_id', 'name')
+            .populate('gaps')
+            .populate('groupId');
+    }
+    findOne(id) {
+        return super.findOne(id)
+            .populate('farms.location.prov_id', 'namek')
+            .populate('farms.location.dist_id', 'name')
+            .populate('farms.location.sect_id', 'name')
+            .populate('farms.location.cell_id', 'name')
+            .populate('farms.location.village_id', 'name')
+            .populate('gaps')
+            .populate('groupId');
+
+    }
 }
 
 module.exports.farmVisitScheduleRepository = new FarmVisitScheduleRepository(
--- src/httpConfig/comm.js
+++ src/httpConfig/comm.js
@@ -8,13 +8,13 @@ const commService = axios.create({
   baseURL: url,
   headers: {
     "x-access-key":
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3ZTcyZGE2LWVkMzctNGMxNy1iNThkLTYzNWZiZmIxYTZhNSIsIm5hbWUiOiJTTUFSVCBLVU5HQUhBUkEgU1lTVEVNIiwiaXBfYWRkcmVzcyI6WyIxMDUiLCIxNzkiLCIxMCIsIjMwIl0sImFwaV9iYXNlX3VybCI6Imh0dHBzOi8vc3RhZ2luZy5zbWFydGt1bmdhaGFyYS5ydyIsImVtYWlsIjoieS5tdWdlbmRhQGJrLnJ3IiwiYWNjZXNzX2tleSI6IjRkMDQyNjllLWNiMTktNGVmNi1hZjdhLTgwMzliMDFlNTNlMyIsInN0YXR1cyI6IkFDVElWRSIsImNyZWF0ZWRBdCI6IjIwMjItMDYtMzBUMTI6Mjk6MDkuNjg0WiIsInVwZGF0ZWRBdCI6IjIwMjItMDYtMzBUMTI6Mjk6MDkuNjg0WiIsImlhdCI6MTY1NzIwMDk3NywiZXhwIjoxNjU3Mjg3Mzc3fQ.U_yS7ksFwUgm2-tFqrrLsatpVV6A4v4psMs7p-ZfSOQ",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3ZTcyZGE2LWVkMzctNGMxNy1iNThkLTYzNWZiZmIxYTZhNSIsIm5hbWUiOiJTTUFSVCBLVU5HQUhBUkEgU1lTVEVNIiwiaXBfYWRkcmVzcyI6WyIxMDUiLCIxNzkiLCIxMCIsIjMwIl0sImFwaV9iYXNlX3VybCI6Imh0dHBzOi8vc3RhZ2luZy5zbWFydGt1bmdhaGFyYS5ydyIsImVtYWlsIjoieS5tdWdlbmRhQGJrLnJ3IiwiYWNjZXNzX2tleSI6IjRkMDQyNjllLWNiMTktNGVmNi1hZjdhLTgwMzliMDFlNTNlMyIsInN0YXR1cyI6IkFDVElWRSIsImNyZWF0ZWRBdCI6IjIwMjItMDYtMzBUMTI6Mjk6MDkuNjg0WiIsInVwZGF0ZWRBdCI6IjIwMjItMDYtMzBUMTI6Mjk6MDkuNjg0WiIsImlhdCI6MTY1NzI3MDIyMSwiZXhwIjoxNjU3MzU2NjIxfQ.anoknMVRCB_BEO36i1j9GW8CBLF9_wxhC-mEth21TwE"
   },
 });
 
 commService.interceptors.request.use((configuration) => {
   const { url, method, params, token } = configuration;
-  logger.info({ url, method, params, token }, "Requesting data from Ikofi");
+  logger.info({ url, method, params, token }, "Requesting data from Comm");
 
   return configuration;
 });
