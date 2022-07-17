e08ff7ae1f28e2011712bc9d00660dcd459ffc81
mugenga <rockyyves@gmail.com>
2022-07-17 14:07:12 +0200

    bug

--- src/cron/index.js
+++ src/cron/index.js
@@ -3,7 +3,5 @@ const { claimToken } = require("../services/comm.service");
 
 /* Every 23 hours claim token from Comm service */
 cron.schedule("0 23 * * *", () => {
-  console.log("claiminig token");
   claimToken();
-  console.log("token claimed");
 });
--- src/utils/schemas/location.js
+++ src/utils/schemas/location.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
+const { provinces, districts, sectors, cells, villages } = require('../../database/location/location');
 const Schema = mongoose.Schema;
 
 const locationSchema = new Schema({
-    prov_id: { type: Schema.Types.ObjectId, ref: 'provinces', index: true },
-    dist_id: { type: Schema.Types.ObjectId, ref: 'districts', index: true },
-    sect_id: { type: Schema.Types.ObjectId, ref: 'sectors', index: true },
-    cell_id: { type: Schema.Types.ObjectId, ref: 'cells', index: true },
-    village_id: { type: Schema.Types.ObjectId, ref: 'villages', index: true }
+    prov_id: { type: Schema.Types.ObjectId, ref: provinces, index: true },
+    dist_id: { type: Schema.Types.ObjectId, ref: districts, index: true },
+    sect_id: { type: Schema.Types.ObjectId, ref: sectors, index: true },
+    cell_id: { type: Schema.Types.ObjectId, ref: cells, index: true },
+    village_id: { type: Schema.Types.ObjectId, ref: villages, index: true }
 });
 
 module.exports = locationSchema;
