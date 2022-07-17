562a2722d5443e8d4658ab5bbc03dd3399c353a3
mugenga <rockyyves@gmail.com>
2022-07-17 13:10:44 +0200

    Debug redis

--- src/services/redis.service.js
+++ src/services/redis.service.js
@@ -1,32 +1,32 @@
 const cache = require("../startup/redisconnection");
 
 class RedisService {
-  cacheData = (key, data, time) => {
+  cacheData(key, data, time) {
     return new Promise((resolve, reject) => {
       cache.setex(key, time, JSON.stringify(data), (err) => {
         if (err) reject(err);
         else resolve();
       });
     });
-  };
+  }
 
-  getCachedData = (key) => {
+  getCachedData(key) {
     return new Promise((resolve, reject) => {
       cache.get(key, (err, reply) => {
         if (err) reject(err);
         else resolve(reply);
       });
     });
-  };
+  }
 
-  cachedKeyExists = (key) => {
+  cachedKeyExists(key) {
     return new Promise((resolve, reject) => {
       cache.exists(key, (err, reply) => {
         if (err) reject(err);
         else resolve(reply);
       });
     });
-  };
+  }
 }
 
 module.exports.RedisService = new RedisService();
