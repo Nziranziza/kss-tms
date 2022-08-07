cb900013c586e71f1c1645f8d4499553840c7f73
mugenga <rockyyves@gmail.com>
2022-08-07 16:08:23 +0200

    Merge

--- src/database/schedule/schedule.repository.js
+++ src/database/schedule/schedule.repository.js
@@ -2,6 +2,7 @@ const {ObjectId} = require("mongodb");
 const BaseRepository = require("../../core/library/BaseRepository");
 const {scheduleStatus} = require("../../tools/constants");
 const {Schedule} = require("./schedule");
+const moment = require("moment");
 
 class ScheduleRepository extends BaseRepository {
 
@@ -81,16 +82,16 @@ class ScheduleRepository extends BaseRepository {
         return schedule.save();
     }
 
-    // Get Attendance Summary
+        // Get Attendance Summary
     async attendanceSummary(body) {
         const {
-            trainingId,
-            trainerId,
-            scheduleId,
-            referenceId,
-            location,
-            date,
-            groupId,
+        trainingId,
+        trainerId,
+        scheduleId,
+        referenceId,
+        location,
+        date,
+        groupId,
         } = body;
 
         let locSearchBy = "";
@@ -99,12 +100,94 @@ class ScheduleRepository extends BaseRepository {
         let startDate = "";
         let endDate = "";
         if (date) {
-            startDate = moment(date.from).startOf("day").toDate();
-            endDate = moment(date.to).endOf("day").toDate();
+        startDate = moment(date.from).startOf("day").toDate();
+        endDate = moment(date.to).endOf("day").toDate();
         }
 
+        // Filter statistics by different values
+        const filter = {
+        $match: {
+            ...(trainingId && { trainingId: ObjectId(trainingId) }),
+            ...(trainerId && { "trainer.userId": ObjectId(trainerId) }),
+            ...(scheduleId && { _id: ObjectId(scheduleId) }),
+            ...(referenceId && { referenceId: ObjectId(referenceId) }),
+            ...(location && { [locSearchBy]: ObjectId(location.locationId) }),
+            ...(date && {
+            startTime: { $gte: startDate, $lt: endDate },
+            }),
+        },
+        };
 
+        // Unwind all trainees so we can compute data
+        const unwind = {
+        $unwind: "$trainees",
+        };
+
+        const filterGroups = {
+        $match: {
+            ...(groupId && { "trainees.groupId": groupId }),
+        },
+        };
+
+        // Group by each gender and attendance
+        const group = {
+        $group: {
+            _id: {
+            absence: "$trainees.attended",
+            gender: "$trainees.gender",
+            },
+            Unique: {
+            $addToSet: "$trainees.userId",
+            },
+        },
+        };
+
+        // count by unique gender and absence status
+        const project = {
+        $project: {
+            _id: 0,
+            absence: "$_id.absence",
+            gender: "$_id.gender",
+            unique: { $size: "$Unique" },
+        },
+        };
+
+        // Run query // Query will return 4 objects or less each containing stats for each gender
+        const summary = await this.model.aggregate([
+        filter,
+        unwind,
+        filterGroups,
+        group,
+        project,
+        ]);
+
+        let femaleAbsent = 0;
+        let maleAbsent = 0;
+        let femalePresent = 0;
+        let malePresent = 0;
+
+        // Compile results
+        summary.forEach((data) => {
+        if (data.gender == "M") {
+            if (data.absence === true) malePresent = malePresent + data.unique;
+            else maleAbsent = maleAbsent + data.unique;
+        } else {
+            if (data.absence === true) femalePresent = femalePresent + data.unique;
+            else femaleAbsent = femaleAbsent + data.unique;
+        }
+        });
+
+        return {
+        femaleAbsent,
+        femalePresent,
+        malePresent,
+        maleAbsent,
+        totalAbsent: femaleAbsent + maleAbsent,
+        totalPresent: malePresent + femalePresent,
+        totalInvitees: femaleAbsent + femalePresent + maleAbsent + malePresent,
+        };
     }
+
 
     statistics(body) {
         const preFilter = [
