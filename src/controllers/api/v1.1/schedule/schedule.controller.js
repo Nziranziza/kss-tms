e207d04181ad9874da6d4765f4ac5457b97481d8
mugenga <rockyyves@gmail.com>
2022-08-22 13:57:22 +0200

    chore: fix getting schedules with deleted trainings modules

--- src/controllers/api/v1.1/schedule/schedule.controller.js
+++ src/controllers/api/v1.1/schedule/schedule.controller.js
@@ -52,7 +52,7 @@ class ScheduleController extends BaseController {
     };
     return asyncWrapper(res, async () => {
       const data = await this.repository.customFindAll(body);
-      data.filter(schedule => console.log(schedule.trainingId));
+      data.filter(schedule => schedule.trainingId === null ? console.log("yes"): console.log("no") );
       return responseWrapper({
         res,
         status: statusCodes.OK,
